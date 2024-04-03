99897441ca856972dd9518153af8fc650ab6f029
Omer <farukoncu78@gmail.com>
2024-04-03 13:14:25 +0300

    media querry

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function Header({ todos, setTodos }) {
-
   const [input, setInput] = useState("");
 
   const addTodo = () => {
@@ -23,20 +22,25 @@ function Header({ todos, setTodos }) {
   };
 
   return (
-    <div className="container header w-50">
+    <div className="container mt-5">
       <h1 className="text-center mt-5 p-3 text-danger">Todo App</h1>
-
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Enter new todo..."
-          aria-label="Enter new todo..."
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-        />
-        <Button variant="success" id="button-addon" onClick={addTodo}>
-          Add Todo
-        </Button>
-      </InputGroup>
+      <div className="row justify-content-center">
+        <div className="col-lg-6 col-md-10">
+          <InputGroup className="input-grup mb-3 p-5">
+            <Form.Control
+              className="form-control"
+              placeholder="Enter new todo..."
+              aria-label="Enter new todo..."
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              style={{ height: "50px" }}
+            />
+            <Button variant="danger" id="button-addon" onClick={addTodo}>
+              Add Todo
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,28 +1,37 @@
 import { BsTrash3 } from "react-icons/bs";
 
 const TodoList = ({ todos, setTodos }) => {
-
-    const deleteTodo = (id)  =>{
-        let filteredTodos = todos.filter((todo) => todo.id !== id);
-        setTodos(filteredTodos);
-        localStorage.setItem("todos", JSON.stringify(filteredTodos));
-    };
+  const deleteTodo = (id) => {
+    let filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+    localStorage.setItem("todos", JSON.stringify(filteredTodos));
+  };
 
   return (
-    <div className="todo-list container w-50">
-      <h2 className="text-center m-5">Todos</h2>
-      <ul>
-        {todos?.length ? (
-          todos.map(({id,text}) => 
-          <div className="list d-flex justify-content-between" key={id}>
-              <li className="text-primary fs-3" >{text}</li>
-              <BsTrash3 className="fs-3 text-danger" onClick={()=>deleteTodo(id)}/>
-          </div>
-          )
-        ) : (
-          <p>No tasks yet!</p>
-        )}
-      </ul>
+    <div className=" container todo-list  mt-3">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+          <h2 className="text-center fs-1 text-danger ">Todos</h2>
+          <ul>
+            {todos?.length ? (
+              todos.map(({ id, text }) => (
+                <div
+                  className="list d-flex justify-content-between align-items-center"
+                  key={id}
+                >
+                  <li className="fs-3">{text}</li>
+                  <BsTrash3
+                    className="fs-3 text-danger"
+                    onClick={() => deleteTodo(id)}
+                  />
+                </div>
+              ))
+            ) : (
+              <p className="text-center mt-5 fs-3">No tasks yet!</p>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
